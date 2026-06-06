f9725f237349d00c4696645977b182ff2e4efe28
Neerajsinghgihar <neerajsinghighar@gmail.com>
2026-06-07 01:42:49 +0530

    Initial framework setup

---
+++ src/test/java/com/neeraj/tests/LoginTest.java
@@ -0,0 +1,16 @@
+package com.neeraj.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void verifyLogin() {
+
+        System.out.println("Running Login Test");
+
+        Assert.assertTrue(true);
+
+    }
+}
